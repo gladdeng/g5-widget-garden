8ddb9881939b41b3d6892d1a7b722e874504cf4b
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-28 11:23:44 -0800

    renames layout to static_asset

--- spec/lib/static_asset_spec.rb
+++ spec/lib/static_asset_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe StaticAsset do
   
   describe "class" do
-    let(:assets) { StaticAsset.all }
+    let(:static_assets) { StaticAsset.all }
     
     it "has 3 things" do
-      assets.should have(8).things
+      static_assets.should have(8).things
     end
     
     it "finds the asset" do
@@ -14,30 +14,30 @@ describe StaticAsset do
     end
   end
   describe "instance" do
-    let(:layout) { StaticAsset.find('storage-list') }
-    it "should have a layout" do
-      layout.name.should eq "storage-list"
+    let(:static_asset) { StaticAsset.find('storage-list') }
+    it "should have a static_asset" do
+      static_asset.name.should eq "storage-list"
     end
     
     it "should have a url" do
-      layout.url.should eq "http://localhost:3001/assets/static_assets/storage-list.html"
+      static_asset.url.should eq "http://localhost:3001/assets/static_assets/storage-list.html"
     end
     
     it "has html" do
-      layout.html.should match /Up to 5 x 5/
+      static_asset.html.should match /Up to 5 x 5/
     end
     
     it "doesn't include stuff outside of widget" do
-      layout.html.should_not match /Storage Unit List/
+      static_asset.html.should_not match /Storage Unit List/
     end
     
     it "has the css" do
-      layout.css.should include "http://g5-web-widgets.herokuapp.com/shared/css/styles.css"
+      static_asset.css.should include "http://g5-web-widgets.herokuapp.com/shared/css/styles.css"
     end
     
     
     it "has js" do
-      layout.js.should include "http://g5-web-widgets.herokuapp.com/shared/js/script.js"
+      static_asset.js.should include "http://g5-web-widgets.herokuapp.com/shared/js/script.js"
     end
     
   end
