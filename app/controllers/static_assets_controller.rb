c512255865009e0c1d7b80a38a090833ebc91d47
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-29 17:16:29 -0800

    some clean up

--- app/controllers/static_assets_controller.rb
+++
@@ -1,9 +0,0 @@
-class StaticAssetsController < ApplicationController
-  def index
-    @static_assets = StaticAsset.all
-  end
-  
-  def show
-    @static_asset = StaticAsset.find(params[:file_name])
-  end
-end
