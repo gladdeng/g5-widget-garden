893ac1cfaf003469c13f4c29670cd7e8a38b3379
Jessica Dillon <g5dev+jd@g5searchmarketing.com>
2013-05-20 15:10:27 -0600

    Renames grunt destination files to save to the appropriate directory for widgets

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,9 +10,19 @@ module.exports = function(grunt) {
           {
             expand: true,
             cwd: '<%= widgetPath %>',
-            src: ['<%= relativeCoffeePath %>/show.js.coffee', '<%= relativeCoffeePath %>/edit.js.coffee'],
-            dest: '<%= widgetPath %>',
-            ext: '.js'
+            src: '<%= relativeCoffeePath %>/*.js.coffee',
+            dest: '<%= widgetPath %>/',
+            ext: '.js',
+            rename: function(dest, src){
+              // Split the src into an array of path components
+              srcComponents = src.split("/")
+              // Cut out the second to last one
+              srcComponents.splice(srcComponents.length - 2, 1)
+              // Join the path components back together into a path
+              newSrc = srcComponents.join("/")
+
+              return dest + newSrc
+            }
           }
         ]
       }
