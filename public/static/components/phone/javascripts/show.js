50833e9be6d70d147b80a7845a31a23f04cdb3bf
Chad Crissman <cdcrissman@gmail.com>
2013-09-23 12:50:15 -0700

    add fields for client and location urns

--- public/static/components/phone/javascripts/show.js
+++ public/static/components/phone/javascripts/show.js
@@ -1,29 +1,29 @@
 (function() {
   $(function() {
-    var client_name, client_urn, location_name, location_urn, row_id;
-    alert('wtf?');
-    $(".p-tel").css("visibility", "hidden");
-    client_urn = "6bqypn2";
-    client_name = "farmhouse";
-    location_urn = "6bqypn2";
-    location_name = "hollywood";
-    row_id = "#" + location_urn + "-" + location_name;
-    return $.get("http://g5-cpns-" + client_urn + "-" + client_name + ".herokuapp.com", function(data) {
-      var $data, numbers, phone, screen;
-      $data = $(data);
-      numbers = $data.find(row_id);
-      screen = document.documentElement.clientWidth;
-      phone = void 0;
-      if (localStorage["ppc"]) {
-        phone = numbers.find(".p-tel-ppc").val();
-      } else if (screen < 768) {
-        phone = numbers.find(".p-tel-mobile").val();
-      } else {
-        phone = numbers.find(".p-tel-default").val();
-      }
-      $(".phone.widget").attr("href", "tel://" + phone).find(".p-tel").html(phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
-      return $(".p-tel").css("visibility", "visible");
-    });
+    var client_urn, location_urn, phoneOptions, row_id;
+    phoneOptions = JSON.parse($('#phone-number-config:first').html());
+    client_urn = phoneOptions["clientUrn"];
+    location_urn = phoneOptions["locationUrn"];
+    if (client_urn && location_urn) {
+      $(".p-tel").css("visibility", "hidden");
+      row_id = "#" + location_urn;
+      return $.get("http://g5-cpns-" + client_urn + ".herokuapp.com", function(data) {
+        var $data, numbers, phone, screen;
+        $data = $(data);
+        numbers = $data.find(row_id);
+        screen = document.documentElement.clientWidth;
+        phone = void 0;
+        if (localStorage["ppc"]) {
+          phone = numbers.find(".p-tel-ppc").val();
+        } else if (screen < 768) {
+          phone = numbers.find(".p-tel-mobile").val();
+        } else {
+          phone = numbers.find(".p-tel-default").val();
+        }
+        $(".phone.widget").attr("href", "tel://" + phone).find(".p-tel").html(phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
+        return $(".p-tel").css("visibility", "visible");
+      });
+    }
   });
 
 }).call(this);
--- public/static/components/phone/javascripts/show/show.js.coffee
+++ public/static/components/phone/javascripts/show/show.js.coffee
@@ -1,23 +1,27 @@
 $ ->
-  $(".p-tel").css "visibility", "hidden"
+  phoneOptions = JSON.parse($('#phone-number-config:first').html())
 
-  client_urn = "6bqypn2-farmhouse"
-  location_urn = "6bqypn2-hollywood"
-  row_id = "#" + location_urn
+  client_urn = phoneOptions["clientUrn"]
+  location_urn = phoneOptions["locationUrn"]
 
-  $.get "http://g5-cpns-" + client_urn + ".herokuapp.com", (data) ->
+  if client_urn && location_urn
+    $(".p-tel").css "visibility", "hidden"
 
-    $data = $(data)
-    numbers = $data.find(row_id)
-    screen = document.documentElement.clientWidth
-    phone = undefined
+    row_id = "#" + location_urn
 
-    if localStorage["ppc"]
-      phone = numbers.find(".p-tel-ppc").val()
-    else if screen < 768
-      phone = numbers.find(".p-tel-mobile").val()
-    else
-      phone = numbers.find(".p-tel-default").val()
+    $.get "http://g5-cpns-" + client_urn + ".herokuapp.com", (data) ->
 
-    $(".phone.widget").attr("href", "tel://" + phone).find(".p-tel").html phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")
-    $(".p-tel").css "visibility", "visible"
+      $data = $(data)
+      numbers = $data.find(row_id)
+      screen = document.documentElement.clientWidth
+      phone = undefined
+
+      if localStorage["ppc"]
+        phone = numbers.find(".p-tel-ppc").val()
+      else if screen < 768
+        phone = numbers.find(".p-tel-mobile").val()
+      else
+        phone = numbers.find(".p-tel-default").val()
+
+      $(".phone.widget").attr("href", "tel://" + phone).find(".p-tel").html phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")
+      $(".p-tel").css "visibility", "visible"
