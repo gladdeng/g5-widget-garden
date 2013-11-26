6261b9ca28dee7e4fab697432600d8a54ff30b4f
jessicard <jessicard@mac.com>
2013-11-25 17:41:00 -0800

    fixes imgs and adds follow btn

--- public/static/components/twitter-feed/javascripts/show.js
+++ public/static/components/twitter-feed/javascripts/show.js
@@ -2,9 +2,6 @@
   $(function() {
     var tweetOptions;
     tweetOptions = JSON.parse($('#twitter-feed-config:first').html());
-    if (tweetOptions.avatar !== true) {
-      $('.tweet-avatar').hide();
-    }
     return $.ajax({
       url: "https://mobile.twitter.com/" + tweetOptions.id,
       dataType: "html",
@@ -13,15 +10,18 @@
       var avatar, tweets;
       tweets = $(".timeline .tweet:lt(" + tweetOptions.count + ")", data.results[0]).toArray();
       avatar = $(".avatar:lt(1) img", data.results[0]);
-      $('.tweet-avatar').attr('src', $(avatar[0]).attr('src'));
       return tweets.forEach(function(tweet) {
-        var tweetTemplate, tweetText, tweetTime, tweetTimestamp, tweetUrl;
+        var tweetAvatar, tweetTemplate, tweetText, tweetTime, tweetTimestamp, tweetUrl;
         tweetText = $(tweet).find(".tweet-text p").text();
         tweetTimestamp = $(tweet).find(".timestamp");
         tweetTime = tweetTimestamp.text();
+        tweetAvatar = $(avatar[0]).attr('src');
         tweetUrl = "http://www.twitter.com" + tweetTimestamp.find("a").attr("href");
-        tweetTemplate = "<span class='tweet-text'>" + tweetText + "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + "</a></span>";
-        return $('.twitter-feed').append(tweetTemplate);
+        tweetTemplate = "<img class='tweet-avatar' src='" + tweetAvatar + "'/> <span class='tweet-text'>" + tweetText + "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + " ago</a></span>";
+        if (tweetOptions.avatar !== true) {
+          $('.tweet-avatar').hide();
+        }
+        return $('.tweet-list').append(tweetTemplate);
       });
     });
   });
--- public/static/components/twitter-feed/javascripts/show/show.js.coffee
+++ public/static/components/twitter-feed/javascripts/show/show.js.coffee
@@ -1,8 +1,6 @@
 $ ->
   tweetOptions = JSON.parse($('#twitter-feed-config:first').html())
 
-  $('.tweet-avatar').hide() unless tweetOptions.avatar is true
-
   $.ajax(
     url: "https://mobile.twitter.com/" + tweetOptions.id
     dataType: "html"
@@ -11,16 +9,16 @@ $ ->
     tweets = $(".timeline .tweet:lt(" + tweetOptions.count + ")", data.results[0]).toArray()
     avatar = $(".avatar:lt(1) img", data.results[0])
 
-    $('.tweet-avatar').attr('src', $(avatar[0]).attr('src'))
-
     tweets.forEach (tweet) ->
       tweetText = $(tweet).find(".tweet-text p").text()
       tweetTimestamp = $(tweet).find(".timestamp")
       tweetTime = tweetTimestamp.text()
+      tweetAvatar = $(avatar[0]).attr('src')
       tweetUrl = "http://www.twitter.com" + tweetTimestamp.find("a").attr("href")
 
-      tweetTemplate = "<span class='tweet-text'>" + tweetText +
-      "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + "</a></span>"
+      tweetTemplate = "<img class='tweet-avatar' src='" + tweetAvatar + "'/> <span class='tweet-text'>" + tweetText +
+      "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + " ago</a></span>"
 
-      $('.twitter-feed').append(tweetTemplate)
+      $('.tweet-avatar').hide() unless tweetOptions.avatar is true
+      $('.tweet-list').append(tweetTemplate)
 
