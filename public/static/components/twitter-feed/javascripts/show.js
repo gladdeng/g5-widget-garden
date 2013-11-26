8cd4619034563541bf6d794d2a8762046d44ec5f
jessicard <jessicard@mac.com>
2013-11-25 17:13:01 -0800

    scrapes for tweets

--- public/static/components/twitter-feed/javascripts/show.js
+++ public/static/components/twitter-feed/javascripts/show.js
@@ -2,8 +2,28 @@
   $(function() {
     var tweetOptions;
     tweetOptions = JSON.parse($('#twitter-feed-config:first').html());
-    tweetOptions.template = tweetOptions.template.replace(/\\/g, "");
-    return getTwitters('tweets', tweetOptions);
+    if (tweetOptions.avatar !== true) {
+      $('.tweet-avatar').hide();
+    }
+    return $.ajax({
+      url: "https://mobile.twitter.com/" + tweetOptions.id,
+      dataType: "html",
+      type: "GET"
+    }).done(function(data) {
+      var avatar, tweets;
+      tweets = $(".timeline .tweet:lt(" + tweetOptions.count + ")", data.results[0]).toArray();
+      avatar = $(".avatar:lt(1) img", data.results[0]);
+      $('.tweet-avatar').attr('src', $(avatar[0]).attr('src'));
+      return tweets.forEach(function(tweet) {
+        var tweetTemplate, tweetText, tweetTime, tweetTimestamp, tweetUrl;
+        tweetText = $(tweet).find(".tweet-text p").text();
+        tweetTimestamp = $(tweet).find(".timestamp");
+        tweetTime = tweetTimestamp.text();
+        tweetUrl = "http://www.twitter.com" + tweetTimestamp.find("a").attr("href");
+        tweetTemplate = "<span class='tweet-text'>" + tweetText + "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + "</a></span>";
+        return $('.twitter-feed').append(tweetTemplate);
+      });
+    });
   });
 
 }).call(this);
--- public/static/components/twitter-feed/javascripts/show/show.js.coffee
+++ public/static/components/twitter-feed/javascripts/show/show.js.coffee
@@ -1,4 +1,26 @@
 $ ->
   tweetOptions = JSON.parse($('#twitter-feed-config:first').html())
-  tweetOptions.template = tweetOptions.template.replace(/\\/g, "")
-  getTwitters('tweets', tweetOptions)
+
+  $('.tweet-avatar').hide() unless tweetOptions.avatar is true
+
+  $.ajax(
+    url: "https://mobile.twitter.com/" + tweetOptions.id
+    dataType: "html"
+    type: "GET"
+  ).done (data) ->
+    tweets = $(".timeline .tweet:lt(" + tweetOptions.count + ")", data.results[0]).toArray()
+    avatar = $(".avatar:lt(1) img", data.results[0])
+
+    $('.tweet-avatar').attr('src', $(avatar[0]).attr('src'))
+
+    tweets.forEach (tweet) ->
+      tweetText = $(tweet).find(".tweet-text p").text()
+      tweetTimestamp = $(tweet).find(".timestamp")
+      tweetTime = tweetTimestamp.text()
+      tweetUrl = "http://www.twitter.com" + tweetTimestamp.find("a").attr("href")
+
+      tweetTemplate = "<span class='tweet-text'>" + tweetText +
+      "<a href=" + tweetUrl + " class='tweet-date' target='_blank'>" + tweetTime + "</a></span>"
+
+      $('.twitter-feed').append(tweetTemplate)
+
