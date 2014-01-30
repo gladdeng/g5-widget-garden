6c1079b82c0b638be25362890702291e8ef96978
Chad Crissman <cdcrissman@gmail.com>
2014-01-30 15:22:42 -0500

    better js for forms

--- public/javascripts/libs/form-urls.js
+++ public/javascripts/libs/form-urls.js
@@ -23,17 +23,17 @@
 
   })();
 
-  canonicalUrl = (function() {
+  var canonicalUrl = function() {
     var inputs = $('input.u-canonical');
     var loc = $(location).attr('href');
     inputs.val(loc);
-  })();
+  };
 
   $(function() {
     var clientUrn;
     clientUrn = JSON.parse($('.g5-enhanced-form .config:first').html());
     new clientLeadsService(clientUrn);
-    new canonicalUrl
+    canonicalUrl();
   });
 
 }).call(this);
