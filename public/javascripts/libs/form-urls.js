ed01bdac95352ac9e099c5526c8fcaf3e47c9233
Chad Crissman <cdcrissman@gmail.com>
2014-01-30 13:48:54 -0500

    adds page url to canonical form input

---
+++ public/javascripts/libs/form-urls.js
@@ -0,0 +1,39 @@
+(function() {
+  var clientLeadsService;
+
+  clientLeadsService = (function() {
+    function clientLeadsService(clientUrn) {
+      var cls_urn, domain, heroku_app_name_max_length;
+      heroku_app_name_max_length = 30;
+      cls_urn = clientUrn["clientUrn"].replace(/-c-/, "-cls-");
+      cls_urn = cls_urn.substring(0, heroku_app_name_max_length);
+      domain = "herokuapp.com";
+      if (cls_urn !== "") {
+        this.formSubmitAction(cls_urn, domain);
+      }
+    }
+
+    clientLeadsService.prototype.formSubmitAction = function(cls_urn, domain) {
+      var clientLeadsUrl;
+      clientLeadsUrl = "http://" + cls_urn + "." + domain + "/html_forms";
+      return $('.g5-enhanced-form form').attr("action", clientLeadsUrl);
+    };
+
+    return clientLeadsService;
+
+  })();
+
+  canonicalUrl = (function() {
+    var inputs = $('[rel=canonical]');
+    var loc = $(location).attr('href');
+    inputs.val(loc);
+  })();
+
+  $(function() {
+    var clientUrn;
+    clientUrn = JSON.parse($('.g5-enhanced-form .config:first').html());
+    new clientLeadsService(clientUrn);
+    new canonicalUrl
+  });
+
+}).call(this);
