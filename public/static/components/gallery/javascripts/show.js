f6139466370eae2bc738194d49be247b30e41ca0
Chad Crissman <cdcrissman@gmail.com>
2014-02-07 17:53:21 -0500

    set height of slideshow container and images

--- public/static/components/gallery/javascripts/show.js
+++ public/static/components/gallery/javascripts/show.js
@@ -1,27 +1,27 @@
 (function() {
-  $(window).load(function() {
-    var flex_height, images, max_height, slider, window_height;
-    window_height = $(window).height();
+  $(function() {
+    var flex_height, images, max_height, slider, slides, window_height;
     slider = $('.flexslider');
-    images = slider.find('li img');
+    slides = slider.find('li').addClass('loading');
+    images = slides.find('img');
+    window_height = $(window).height();
     max_height = 0;
     flex_height = 0;
-    images.each(function() {
-      return $(this).on('load', function() {
-        var cur_height;
-        cur_height = $(this).height();
-        if (cur_height > max_height) {
-          max_height = cur_height;
-        }
-        if (window_height < max_height) {
-          flex_height = window_height - 20;
-        } else {
-          flex_height = max_height - 20;
-        }
-        slider.css('height', flex_height);
-        return images.css('max-height', flex_height);
-      });
+    slides.each(function() {
+      var cur_height;
+      cur_height = $(this).find('img').height();
+      if (cur_height > max_height) {
+        return max_height = cur_height;
+      }
     });
+    slides.removeClass('loading');
+    if (window_height < max_height) {
+      flex_height = window_height - 20;
+    } else {
+      flex_height = max_height - 20;
+    }
+    slider.css('height', flex_height);
+    images.css('max-height', flex_height);
     return slider.flexslider({
       "animation": "fade",
       "useCSS": true,
--- public/static/components/gallery/javascripts/show/show.js.coffee
+++ public/static/components/gallery/javascripts/show/show.js.coffee
@@ -1,26 +1,28 @@
-$(window).load ->
-  window_height = $(window).height()
+$ ->
   slider = $('.flexslider')
-  images = slider.find('li img')
+  slides = slider.find('li').addClass('loading')
+  images = slides.find('img')
+  window_height = $(window).height()
   max_height = 0
   flex_height = 0
 
   # Get height of tallest image
-  images.each ->
-    $(this).on 'load', ->
-      cur_height = $(this).height()
-      if cur_height > max_height
-        max_height = cur_height
+  slides.each ->
+    cur_height = $(this).find('img').height()
+    if cur_height > max_height
+      max_height = cur_height
+
+  slides.removeClass('loading')
 
   # Compare window height to tallest image height
-      if window_height < max_height
-        flex_height = window_height-20
-      else
-        flex_height = max_height-20
+  if window_height < max_height
+    flex_height = window_height-20
+  else
+    flex_height = max_height-20
 
   # Set container height and max image height
-      slider.css('height', flex_height)
-      images.css('max-height', flex_height)
+  slider.css('height', flex_height)
+  images.css('max-height', flex_height)
 
   slider.flexslider
     "animation": "fade"
