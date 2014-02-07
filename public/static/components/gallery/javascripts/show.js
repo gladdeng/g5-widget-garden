651a461c538068f0f9ea38010354d6309bba72db
Chad Crissman <cdcrissman@gmail.com>
2014-02-07 16:41:48 -0500

    wip sets max height of slideshow

--- public/static/components/gallery/javascripts/show.js
+++ public/static/components/gallery/javascripts/show.js
@@ -1,8 +1,31 @@
 (function() {
   $(window).load(function() {
-    var galleryOptions;
-    galleryOptions = JSON.parse($(".gallery .config:first").html());
-    return $(".flexslider").flexslider(galleryOptions);
+    var flex_height, images, max_height, slider, window_height;
+    window_height = $(window).height();
+    slider = $('.flexslider');
+    images = slider.find('li img');
+    max_height = 0;
+    flex_height = 0;
+    images.each(function() {
+      var cur_height;
+      cur_height = $(this).height();
+      if (cur_height > max_height) {
+        return max_height = cur_height;
+      }
+    });
+    if (window_height < max_height) {
+      flex_height = window_height - 20;
+    } else {
+      flex_height = max_height - 20;
+    }
+    slider.css('height', flex_height);
+    images.css('max-height', flex_height);
+    return slider.flexslider({
+      "animation": "fade",
+      "useCSS": true,
+      "touch": true,
+      "directionNav": true
+    });
   });
 
 }).call(this);
--- public/static/components/gallery/javascripts/show/show.js.coffee
+++ public/static/components/gallery/javascripts/show/show.js.coffee
@@ -1,4 +1,28 @@
-# http://www.woothemes.com/flexslider/
 $(window).load ->
-  galleryOptions = JSON.parse($(".gallery .config:first").html())
-  $(".flexslider").flexslider galleryOptions
+  window_height = $(window).height()
+  slider = $('.flexslider')
+  images = slider.find('li img')
+  max_height = 0
+  flex_height = 0
+
+  # Get height of tallest image
+  images.each ->
+    cur_height = $(this).height()
+    if cur_height > max_height
+      max_height = cur_height
+
+  # Compare window height to tallest image height
+  if window_height < max_height
+    flex_height = window_height-20
+  else
+    flex_height = max_height-20
+
+  # Set container height and max image height
+  slider.css('height', flex_height)
+  images.css('max-height', flex_height)
+
+  slider.flexslider
+    "animation": "fade"
+    "useCSS": true
+    "touch": true
+    "directionNav": true
