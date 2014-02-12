5b6655f928db91390127748c8d1a169929fab7ad
Chad Crissman <cdcrissman@gmail.com>
2014-02-11 19:35:45 -0500

    wait for images to load before setting floorplan height

--- public/static/components/floorplans/javascripts/show.js
+++ public/static/components/floorplans/javascripts/show.js
@@ -1,5 +1,5 @@
 (function() {
-  var pricingAndAvailability;
+  var appendFloorplans, pricingAndAvailability, setPricingHeight, setupFilters;
 
   pricingAndAvailability = (function() {
     function pricingAndAvailability(pricingOptions) {
@@ -20,46 +20,10 @@
       loader = '<div id="loading-floorplans"><div class="loader">Loading&hellip;</div>Loading Pricing &amp; Availibility Information&hellip;</div>';
       return $.ajax({
         type: "GET",
-        url: pricingURL,
-        success: function(data) {
-          var $data, floorplanList, floorplans, floorplansHeight;
-          floorplanContainer.hide();
-          $("[role=main]").append(loader);
-          $data = $(data);
-          floorplanList = $data.find(".e-content");
-          floorplanContainer.append(floorplanList).fadeIn();
-          $("#loading-floorplans").fadeOut().remove();
-          floorplansHeight = floorplanContainer.outerHeight();
-          floorplanContainer.css('height', floorplansHeight);
-          floorplans = $(".floorplan");
-          $('.filters input[type=radio]').each(function() {
-            var klass;
-            klass = $(this).attr('id');
-            if (!(floorplans.hasClass(klass) || klass.match(/^\w+-all/))) {
-              return $(this).prop("disabled", true).next().addClass('disabled');
-            }
-          });
-          $(".filters input").on("change", function(e) {
-            var bathFilter, bathSelector, bedFilter, bedSelector;
-            bedFilter = $("#beds-filter input:checked").val();
-            bathFilter = $("#baths-filter input:checked").val();
-            bedSelector = "";
-            bathSelector = "";
-            if (bedFilter === "beds-all" && bathFilter === "baths-all") {
-              return floorplans.fadeIn();
-            } else {
-              if (bedFilter !== "beds-all") {
-                bedSelector = "." + bedFilter;
-              }
-              if (bathFilter !== "baths-all") {
-                bathSelector = "." + bathFilter;
-              }
-              floorplans.fadeOut();
-              return $(bedSelector + bathSelector).fadeIn("fast");
-            }
-          });
-          return $(".floorplans .floorplan-btn").fancybox();
-        }
+        url: pricingURL
+      }).done(function(data) {
+        appendFloorplans(data, floorplanContainer, loader);
+        return setupFilters();
       });
     };
 
@@ -67,11 +31,73 @@
 
   })();
 
+  appendFloorplans = function(data, floorplanContainer, loader) {
+    var $data, floorplanList, images, loadCounter;
+    floorplanContainer.hide();
+    $("[role=main]").append(loader);
+    $data = $(data);
+    floorplanList = $data.find(".e-content");
+    floorplanContainer.append(floorplanList).fadeIn();
+    $("#loading-floorplans").fadeOut().remove();
+    loadCounter = 0;
+    images = floorplanContainer.find('img');
+    return $.each(images, function(i, item) {
+      return $(item).load(function() {
+        loadCounter++;
+        console.log('image ' + i + 'loaded');
+        if (loadCounter === images.length) {
+          console.log('all images loaded');
+          return setPricingHeight(floorplanContainer);
+        }
+      });
+    });
+  };
+
+  setPricingHeight = function(floorplanContainer) {
+    var floorplansHeight;
+    floorplanContainer = $('.floorplans');
+    floorplansHeight = floorplanContainer.outerHeight();
+    return floorplanContainer.css('height', floorplansHeight);
+  };
+
+  setupFilters = function() {
+    var floorplans;
+    floorplans = $(".floorplan");
+    $('.filters input[type=radio]').each(function() {
+      var klass;
+      klass = $(this).attr('id');
+      if (!(floorplans.hasClass(klass) || klass.match(/^\w+-all/))) {
+        return $(this).prop("disabled", true).next().addClass('disabled');
+      }
+    });
+    return $(".filters input").on("change", function(e) {
+      var bathFilter, bathSelector, bedFilter, bedSelector;
+      bedFilter = $("#beds-filter input:checked").val();
+      bathFilter = $("#baths-filter input:checked").val();
+      bedSelector = "";
+      bathSelector = "";
+      if (bedFilter === "beds-all" && bathFilter === "baths-all") {
+        return floorplans.fadeIn();
+      } else {
+        if (bedFilter !== "beds-all") {
+          bedSelector = "." + bedFilter;
+        }
+        if (bathFilter !== "baths-all") {
+          bathSelector = "." + bathFilter;
+        }
+        floorplans.fadeOut();
+        return $(bedSelector + bathSelector).fadeIn("fast");
+      }
+    });
+  };
+
   $(function() {
     var pricingOptions;
-    $.getScript("http://g5-widget-garden.herokuapp.com/javascripts/libs/fancybox/jquery.fancybox.pack.js");
     pricingOptions = JSON.parse($('.floorplans .config:first').html());
-    return new pricingAndAvailability(pricingOptions);
+    new pricingAndAvailability(pricingOptions);
+    return $.getScript("http://g5-widget-garden.herokuapp.com/javascripts/libs/fancybox/jquery.fancybox.pack.js").done(function() {
+      return $(".floorplans .floorplan-btn").fancybox();
+    });
   });
 
 }).call(this);
