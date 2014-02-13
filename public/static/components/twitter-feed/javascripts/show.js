1b53ee05377901eddd2ba198307cd7df666c097a
Chad Crissman <cdcrissman@gmail.com>
2014-02-13 18:52:01 -0500

    adds user url to twitter name

--- public/static/components/twitter-feed/javascripts/show.js
+++ public/static/components/twitter-feed/javascripts/show.js
@@ -19,23 +19,25 @@
     twitterUrl = "http://www.twitter.com";
     composedTweets = [];
     tweets.forEach(function(tweet) {
-      var avatarUrl, replyHtml, time, timestamp, tweetHtml, url, user, userInfo, userName;
+      var avatarUrl, replyHtml, time, timestamp, tweetHtml, url, user, userInfo, userName, userUrl;
       timestamp = $(tweet).find(".timestamp");
       user = timestamp.find("a").attr("href");
       userName = $(tweet).find('.fullname').html();
       time = timestamp.text();
       avatarUrl = $(avatar[0]).attr('src');
+      userUrl = twitterUrl + '/' + userName;
       url = twitterUrl + user;
       tweetHtml = $(tweet).find(".tweet-text");
       replyHtml = tweetHtml.find(".twitter-atreply");
       if ($(tweet).has('.context').length > 0) {
         userInfo = $(tweet).find('.tweet-header');
         avatarUrl = userInfo.find(".avatar img").attr("src");
+        userName = userInfo.find(".fullname").html();
       }
       replyHtml.each(function() {
         return $(this).attr("href", twitterUrl + $(this).attr("href"));
       });
-      return composedTweets.push(tweetTemplate(avatarUrl, userName, tweetHtml.html(), url, time));
+      return composedTweets.push(tweetTemplate(avatarUrl, userName, userUrl, tweetHtml.html(), url, time));
     });
     if (tweetOptions.avatar !== true) {
       $('.tweet-avatar').hide();
@@ -43,8 +45,8 @@
     return $('.tweet-list').append(composedTweets);
   };
 
-  tweetTemplate = function(avatar, userName, text, url, time) {
-    return "<li><img class='tweet-avatar' src='" + avatar + "'/>  <a href=" + url + " class='tweet-date' target='_blank'>" + time + " ago</a>  <span class='tweet-name'>" + userName + "</span>  <span class='tweet-text'> " + text + "</span></li>";
+  tweetTemplate = function(avatar, userName, userUrl, text, url, time) {
+    return "<li><img class='tweet-avatar' src='" + avatar + "'/>  <a href=" + url + " class='tweet-date' target='_blank'>" + time + " ago</a>  <a href=" + userUrl + " class='tweet-name' target='_blank'>" + userName + "</a>  <span class='tweet-text'> " + text + "</span></li>";
   };
 
   $(function() {
