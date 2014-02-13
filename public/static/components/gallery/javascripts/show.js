66726e82b895055dacd4be8735a7cdd136aa4732
Chad Crissman <cdcrissman@gmail.com>
2014-02-13 14:01:24 -0500

    resize gallery on window resize

--- public/static/components/gallery/javascripts/show.js
+++ public/static/components/gallery/javascripts/show.js
@@ -1,5 +1,38 @@
 (function() {
-  var setupFlexslider;
+  var resetFlexslider, setupFlexslider;
+
+  (function($, sr) {
+    var debounce;
+    debounce = function(func, threshold, execAsap) {
+      var debounced, timeout;
+      timeout = void 0;
+      return debounced = function() {
+        var delayed, obj;
+        delayed = function() {
+          if (!execAsap) {
+            func.apply(obj);
+          }
+          timeout = null;
+        };
+        obj = this;
+        if (timeout) {
+          clearTimeout(timeout);
+        } else {
+          if (execAsap) {
+            func.apply(obj);
+          }
+        }
+        timeout = setTimeout(delayed, threshold || 100);
+      };
+    };
+    jQuery.fn[sr] = function(fn) {
+      if (fn) {
+        return this.bind("resize", debounce(fn));
+      } else {
+        return this.trigger(sr);
+      }
+    };
+  })(jQuery, "smartresize");
 
   setupFlexslider = function() {
     var bottom_space, image_height, images, slides, tallest_image, window_height, wrapper, wrapper_height;
@@ -10,7 +43,7 @@
     tallest_image = 0;
     wrapper_height = 0;
     image_height = 0;
-    bottom_space = 33 + 10;
+    bottom_space = 58 + 10;
     slides.each(function() {
       var cur_height;
       cur_height = $(this).find('img').height();
@@ -30,6 +63,15 @@
     return images.css('max-height', image_height);
   };
 
+  resetFlexslider = function() {
+    var images, wrapper;
+    wrapper = $('.flexslider-container');
+    images = wrapper.find('.slides img');
+    wrapper.css('height', 'auto');
+    images.css('max-height', 'none');
+    return setupFlexslider();
+  };
+
   $(function() {
     var galleryOptions;
     setupFlexslider();
@@ -38,16 +80,10 @@
       animation: galleryOptions['animation'],
       useCSS: true,
       touch: true,
-      directionNav: true,
-      after: function(slider) {
-        var currHeight;
-        currHeight = $(".flexslider").find(".slides > li").eq(slider.currentSlide).outerHeight();
-        console.log('currHeight = ' + currHeight);
-        return $(".slides").css('height', currHeight);
-      }
+      directionNav: true
     });
-    return $(window).resize(function() {
-      return setupFlexslider();
+    return $(window).smartresize(function() {
+      return resetFlexslider();
     });
   });
 
--- public/static/components/gallery/javascripts/show/show.js.coffee
+++ public/static/components/gallery/javascripts/show/show.js.coffee
@@ -1,3 +1,28 @@
+# debouncing function from John Hann
+# http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+(($, sr) ->
+  debounce = (func, threshold, execAsap) ->
+    timeout = undefined
+    debounced = ->
+      delayed = ->
+        func.apply obj  unless execAsap
+        timeout = null
+        return
+      obj = this
+      if timeout
+        clearTimeout timeout
+      else func.apply obj  if execAsap
+      timeout = setTimeout(delayed, threshold or 100)
+      return
+
+  # smartresize
+  jQuery.fn[sr] = (fn) ->
+    (if fn then @bind("resize", debounce(fn)) else @trigger(sr))
+
+  return
+) jQuery, "smartresize"
+
+
 setupFlexslider = ->
   # Slider Elements
   wrapper = $('.flexslider-container')
@@ -9,7 +34,7 @@ setupFlexslider = ->
   tallest_image = 0
   wrapper_height = 0
   image_height = 0
-  bottom_space = 33 + 10
+  bottom_space = 58 + 10
 
   # Get height of tallest image
   slides.each ->
@@ -32,6 +57,16 @@ setupFlexslider = ->
   images.css('max-height', image_height)
 
 
+resetFlexslider = ->
+  wrapper = $('.flexslider-container')
+  images = wrapper.find('.slides img')
+
+  wrapper.css('height', 'auto')
+  images.css('max-height', 'none')
+
+  setupFlexslider()
+
+
 $ ->
   # Set heights of flexslider container and images
   setupFlexslider()
@@ -43,11 +78,7 @@ $ ->
     useCSS: true
     touch: true
     directionNav: true
-    after: (slider) ->
-      currHeight = $(".flexslider").find(".slides > li").eq(slider.currentSlide).outerHeight()
-      console.log('currHeight = ' + currHeight)
-      $(".slides").css 'height', currHeight
 
   # Recalculate slider heights with window change
-  $(window).resize ->
-    setupFlexslider()
+  $(window).smartresize ->
+    resetFlexslider()
