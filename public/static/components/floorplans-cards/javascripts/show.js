fdc44928a24eac904fdc07e235f7f53159b2a17e
johnlucia <johnmlucia@gmail.com>
2014-06-11 11:18:22 -0700

    start pulling in config options

--- public/static/components/floorplans-cards/javascripts/show.js
+++ public/static/components/floorplans-cards/javascripts/show.js
@@ -2,11 +2,13 @@
   var populateUnitData;
 
   populateUnitData = (function() {
-    var bedroomMarkup, buildHTML, url;
+    var bedroomMarkup, buildHTML, floorplanConfig, url;
 
     function populateUnitData() {}
 
-    url = 'http://localhost:3004/locations/abc-123/floor_plans.json';
+    floorplanConfig = JSON.parse($('#floorplan-cards-config').html());
+
+    url = floorplanConfig["floorplanDataFeed"];
 
     $.getJSON(url, function(unitData) {
       return buildHTML(unitData);
@@ -18,7 +20,7 @@
       unitsMarkup = "";
       for (index in unitData) {
         floorplan = unitData[index];
-        unitsMarkup += "<div class='floorplan-card'>                        <div class='floorplan-card-title'>" + floorplan["title"] + "</div>                                                <a href='" + floorplan["image_url"] + "' target='_blank' class='floorplan-view-link'>View</a>                        <div class='unit-details'>                          <div class='unit-beds'>" + (bedroomMarkup(floorplan["beds"])) + "</div>                          <div class='unit-baths'><span>" + floorplan["baths"] + "</span> Bathroom</div>                          <div class='unit-size'>" + floorplan["size"] + " Sq. Ft.</div>                          <div class='unit-rate'>From " + floorplan["price"] + "</div>                        </div>                      </div>";
+        unitsMarkup += "<div class='floorplan-card'>                        <div class='floorplan-card-title'>" + floorplan["title"] + "</div>                                                <a href='" + floorplan["image_url"] + "' target='_blank' class='floorplan-view-link'><span>View</span></a>                        <div class='unit-details'>                          <div class='unit-beds'>" + (bedroomMarkup(floorplan["beds"])) + "</div>                          <div class='unit-baths'><span>" + floorplan["baths"] + "</span> Bathroom</div>                          <div class='unit-size'>" + floorplan["size"] + " Sq. Ft.</div>                          <div class='unit-rate'>From " + floorplan["price"] + "</div>                        </div>                        <a href='#' class='unit-cta-button'>Unit CTA</a>                      </div>";
       }
       return unitsDiv.append(unitsMarkup);
     };
@@ -27,7 +29,7 @@
       if (bedrooms > 0) {
         return "<span>" + bedrooms + "</span> Bedroom";
       } else {
-        return "Studio";
+        return "<span>S</span> Studio";
       }
     };
 
--- public/static/components/floorplans-cards/javascripts/show/show.js.coffee
+++ public/static/components/floorplans-cards/javascripts/show/show.js.coffee
@@ -1,6 +1,6 @@
 class populateUnitData
-  # eventually need to pull this url from the widget variables
-  url = 'http://localhost:3004/locations/abc-123/floor_plans.json'
+  floorplanConfig = JSON.parse($('#floorplan-cards-config').html())
+  url = floorplanConfig["floorplanDataFeed"]
 
   $.getJSON(url, (unitData) -> buildHTML(unitData))
 
@@ -11,13 +11,14 @@ class populateUnitData
       unitsMarkup += "<div class='floorplan-card'>
                         <div class='floorplan-card-title'>#{floorplan["title"]}</div>
                         
-                        <a href='#{floorplan["image_url"]}' target='_blank' class='floorplan-view-link'>View</a>
+                        <a href='#{floorplan["image_url"]}' target='_blank' class='floorplan-view-link'><span>View</span></a>
                         <div class='unit-details'>
                           <div class='unit-beds'>#{bedroomMarkup floorplan["beds"]}</div>
                           <div class='unit-baths'><span>#{floorplan["baths"]}</span> Bathroom</div>
                           <div class='unit-size'>#{floorplan["size"]} Sq. Ft.</div>
                           <div class='unit-rate'>From #{floorplan["price"]}</div>
                         </div>
+                        <a href='#' class='unit-cta-button'>Unit CTA</a>
                       </div>"
 
     unitsDiv.append(unitsMarkup)
@@ -26,6 +27,5 @@ class populateUnitData
     if bedrooms > 0
       "<span>#{bedrooms}</span> Bedroom"
     else
-      "Studio"
+      "<span>S</span> Studio"
 
- 
