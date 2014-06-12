c545afa01dc9d5f2686d85572170948be372a59e
johnlucia <johnmlucia@gmail.com>
2014-06-12 15:42:18 -0700

    so close to done with floorplans cards

--- public/static/components/floorplans-cards/javascripts/show.js
+++ public/static/components/floorplans-cards/javascripts/show.js
@@ -5,22 +5,23 @@
     var bedroomMarkup, buildHTML;
 
     function populateUnitData(floorplanConfig) {
-      var dataFeed;
+      var ctaText, dataFeed;
       dataFeed = floorplanConfig['floorplanDataFeed'];
+      ctaText = floorplanConfig['ctaText'];
       $.getJSON(dataFeed, function(unitData) {
-        return buildHTML(unitData);
+        return buildHTML(unitData, ctaText);
       }).then(function(response) {
         return new customizeUnitGrid(floorplanConfig);
       });
     }
 
-    buildHTML = function(unitData) {
+    buildHTML = function(unitData, ctaText) {
       var floorplan, index, unitsDiv, unitsMarkup;
       unitsDiv = $('.floorplans-cards').first();
       unitsMarkup = "";
       for (index in unitData) {
         floorplan = unitData[index];
-        unitsMarkup += "<div class='floorplan-card'>                        <div class='floorplan-card-title'>" + floorplan["title"] + "</div>                        <a href='" + floorplan["image_url"] + "' target='_blank' class='floorplan-view-link'>                          <div>View<span></span></div>                        </a>                        <div class='unit-details'>                          <div class='unit-beds'>" + (bedroomMarkup(floorplan["beds"])) + "</div>                          <div class='unit-baths'><span>" + floorplan["baths"] + "</span> Bathroom</div>                          <div class='unit-size'>" + floorplan["size"] + " Sq. Ft.</div>                          <div class='unit-rate'>From " + floorplan["price"] + "</div>                        </div>                        <a href='#' class='unit-cta-button'>Unit CTA</a>                      </div>";
+        unitsMarkup += "<div class='floorplan-card'>                        <div class='floorplan-card-title'>" + floorplan["title"] + "</div>                        <a href='" + floorplan["image_url"] + "' class='floorplan-view-link'>                          <div>View<span></span></div>                        </a>                        <div class='unit-details'>                          <div class='unit-beds'>" + (bedroomMarkup(floorplan["beds"])) + "</div>                          <div class='unit-baths'><span>" + floorplan["baths"] + "</span> Bathroom</div>                          <div class='unit-size'>" + floorplan["size"] + " Sq. Ft.</div>                          <div class='unit-rate'>From <span>$" + floorplan["price"] + "</span></div>                        </div>                        <a href='#' class='unit-cta-button'>" + ctaText + "</a>                      </div>";
       }
       return unitsDiv.append(unitsMarkup);
     };
@@ -45,6 +46,7 @@
       setCtaColor(colorConfigurations['ctaColor'], colorConfigurations['accentColor1']);
       setAccents1(colorConfigurations['accentColor1']);
       setAccents2(colorConfigurations['accentColor2']);
+      $(".floorplan-view-link").fancybox();
     }
 
     setHeadingColor = function(color) {
--- public/static/components/floorplans-cards/javascripts/show/show.js.coffee
+++ public/static/components/floorplans-cards/javascripts/show/show.js.coffee
@@ -1,25 +1,26 @@
 class populateUnitData
   constructor: (floorplanConfig) -> 
     dataFeed = floorplanConfig['floorplanDataFeed']
-    $.getJSON(dataFeed, (unitData) -> buildHTML(unitData)).then (response) ->
+    ctaText = floorplanConfig['ctaText']
+    $.getJSON(dataFeed, (unitData) -> buildHTML(unitData, ctaText)).then (response) ->
       new customizeUnitGrid(floorplanConfig)
 
-  buildHTML = (unitData) ->
+  buildHTML = (unitData, ctaText) ->
     unitsDiv = $('.floorplans-cards').first()
     unitsMarkup = ""
     for index, floorplan of unitData
       unitsMarkup += "<div class='floorplan-card'>
                         <div class='floorplan-card-title'>#{floorplan["title"]}</div>
-                        <a href='#{floorplan["image_url"]}' target='_blank' class='floorplan-view-link'>
+                        <a href='#{floorplan["image_url"]}' class='floorplan-view-link'>
                           <div>View<span></span></div>
                         </a>
                         <div class='unit-details'>
                           <div class='unit-beds'>#{bedroomMarkup floorplan["beds"]}</div>
                           <div class='unit-baths'><span>#{floorplan["baths"]}</span> Bathroom</div>
                           <div class='unit-size'>#{floorplan["size"]} Sq. Ft.</div>
-                          <div class='unit-rate'>From #{floorplan["price"]}</div>
+                          <div class='unit-rate'>From <span>$#{floorplan["price"]}</span></div>
                         </div>
-                        <a href='#' class='unit-cta-button'>Unit CTA</a>
+                        <a href='#' class='unit-cta-button'>#{ctaText}</a>
                       </div>"
 
     unitsDiv.append(unitsMarkup)
@@ -36,6 +37,7 @@ class customizeUnitGrid
     setCtaColor colorConfigurations['ctaColor'], colorConfigurations['accentColor1']
     setAccents1 colorConfigurations['accentColor1']
     setAccents2 colorConfigurations['accentColor2']
+    $(".floorplan-view-link").fancybox()
 
   setHeadingColor = (color) ->
     $('.floorplan-card-title').css('background-color', color)
