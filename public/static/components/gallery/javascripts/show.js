6c8fe837b8f601d9fdc8b56710160180dbac9824
Chad Crissman <cdcrissman@gmail.com>
2014-07-08 14:31:52 -0400

    slight js and style refactor

--- public/static/components/gallery/javascripts/show.js
+++ public/static/components/gallery/javascripts/show.js
@@ -1,11 +1,29 @@
 (function() {
-  var gallery, getTallestCaption, getTallestImage, initializeFlexSlider, resetFlexslider, resetMiniFlexslider, setImageHeight, setMiniNavHeight, setupFlexslider;
+  var gallery, getTallestImage, initializeFlexSlider, resetFlexslider, resetMiniFlexslider, setImageHeight, setMiniNavHeight, setupFlexslider;
 
   gallery = {
     flexContainer: $('.flexslider'),
     wrapper: $('.slides'),
     slides: $('.slides li'),
     images: $('.slides img')
+  };
+
+  initializeFlexSlider = function(galleryOptions) {
+    var captionHeight, navHeight, showThumbs;
+    showThumbs = (galleryOptions['show_thumbnails'] === "yes" ? "thumbnails" : true);
+    gallery.flexContainer.flexslider({
+      animation: galleryOptions['animation'],
+      useCSS: true,
+      touch: true,
+      directionNav: true,
+      controlNav: showThumbs
+    });
+    if (galleryOptions['mini_gallery'] === 'no') {
+      navHeight = gallery.flexContainer.find('.flex-control-nav').outerHeight();
+      captionHeight = getTallestCaption();
+      gallery.flexContainer.find('.flex-control-nav').css('bottom', -navHeight);
+      return gallery.flexContainer.css('margin-bottom', -navHeight);
+    }
   };
 
   getTallestImage = function() {
@@ -25,57 +43,21 @@
     return tallestImage;
   };
 
-  getTallestCaption = function() {
-    var tallestCaption;
-    gallery.slides.addClass('loading');
-    tallestCaption = 0;
-    gallery.flexContainer.find('.flex-caption').each(function() {
-      var curHeight;
-      curHeight = null;
-      curHeight = $(this).outerHeight(true);
-      if (curHeight > tallestCaption) {
-        return tallestCaption = curHeight;
-      }
-    });
-    gallery.slides.removeClass('loading');
-    return tallestCaption;
-  };
-
-  initializeFlexSlider = function(galleryOptions) {
-    var bottomSpace, captionHeight, navHeight, showThumbs;
-    showThumbs = (galleryOptions['show_thumbnails'] === "yes" ? "thumbnails" : true);
-    gallery.flexContainer.flexslider({
-      animation: galleryOptions['animation'],
-      useCSS: true,
-      touch: true,
-      directionNav: true,
-      controlNav: showThumbs
-    });
-    if (galleryOptions['mini_gallery'] === 'no') {
-      navHeight = gallery.flexContainer.find('.flex-control-nav').outerHeight();
-      captionHeight = getTallestCaption();
-      bottomSpace = navHeight + captionHeight;
-      gallery.flexContainer.find('.flex-control-nav').css('bottom', -bottomSpace);
-      return gallery.flexContainer.css('margin-bottom', -bottomSpace);
-    }
-  };
-
   setImageHeight = function(tallestImage) {
-    var bottomSpace, captionHeight, fixedHeight, navHeight, padding, windowHeight;
+    var fixedHeight, navHeight, padding, windowHeight;
     windowHeight = $(window).height();
     navHeight = gallery.flexContainer.find('.flex-control-nav').outerHeight(true);
-    captionHeight = getTallestCaption();
-    bottomSpace = navHeight + captionHeight;
     fixedHeight = null;
     padding = 10;
-    if (windowHeight <= tallestImage + bottomSpace) {
+    if (windowHeight <= tallestImage + navHeight) {
       fixedHeight = windowHeight - navHeight - padding;
     } else {
       fixedHeight = tallestImage - padding;
     }
     gallery.images.css('max-height', fixedHeight);
     gallery.slides.css('height', fixedHeight);
-    return gallery.flexContainer.css('margin-bottom', bottomSpace);
+    gallery.flexContainer.find('.flex-control-nav').css('bottom', -navHeight);
+    return gallery.flexContainer.css('margin-bottom', navHeight);
   };
 
   setMiniNavHeight = function(tallestImage) {
@@ -86,11 +68,10 @@
     var tallestImage;
     tallestImage = getTallestImage();
     initializeFlexSlider(galleryOptions);
-    if (galleryOptions['mini_gallery'] === 'no') {
-      setImageHeight(tallestImage);
-    }
     if (galleryOptions['mini_gallery'] === 'yes') {
       return setMiniNavHeight(tallestImage);
+    } else {
+      return setImageHeight(tallestImage);
     }
   };
 
@@ -110,14 +91,13 @@
     var galleryOptions;
     galleryOptions = JSON.parse($('.gallery .config:first').html());
     setupFlexslider(galleryOptions);
-    if (galleryOptions['mini_gallery'] === 'no') {
-      $(window).smartresize(function() {
-        return resetFlexslider();
-      });
-    }
     if (galleryOptions['mini_gallery'] === 'yes') {
       return $(window).smartresize(function() {
         return resetMiniFlexslider();
+      });
+    } else {
+      return $(window).smartresize(function() {
+        return resetFlexslider();
       });
     }
   });
--- public/static/components/gallery/javascripts/show/show.js.coffee
+++ public/static/components/gallery/javascripts/show/show.js.coffee
@@ -5,31 +5,6 @@ gallery =
   slides: $('.slides li')
   images: $('.slides img')
 
-# Gets the height of the tallest image
-getTallestImage = ->
-  gallery.slides.addClass 'loading'
-  gallery.images.css 'max-height', 'none'
-  tallestImage = 0
-  gallery.images.each ->
-    curHeight = null
-    curHeight = $(this).height()
-    tallestImage = curHeight  if curHeight > tallestImage
-
-  gallery.slides.removeClass 'loading'
-  tallestImage
-
-# Gets the height of the tallest caption
-getTallestCaption = ->
-  gallery.slides.addClass 'loading'
-  tallestCaption = 0
-  gallery.flexContainer.find('.flex-caption').each ->
-    curHeight = null
-    curHeight = $(this).outerHeight(true)
-    tallestCaption = curHeight  if curHeight > tallestCaption
-
-  gallery.slides.removeClass 'loading'
-  tallestCaption
-
 # Creates the slideshow
 initializeFlexSlider = (galleryOptions) ->
   showThumbs = (if galleryOptions['show_thumbnails'] is "yes" then "thumbnails" else true)
@@ -46,28 +21,38 @@ initializeFlexSlider = (galleryOptions) ->
     # Set placement of gallery nav based on its height
     navHeight = gallery.flexContainer.find('.flex-control-nav').outerHeight()
     captionHeight = getTallestCaption()
-    bottomSpace = navHeight + captionHeight
-    gallery.flexContainer.find('.flex-control-nav').css('bottom', -bottomSpace)
-    gallery.flexContainer.css 'margin-bottom', -bottomSpace
+    gallery.flexContainer.find('.flex-control-nav').css('bottom', -navHeight)
+    gallery.flexContainer.css 'margin-bottom', -navHeight
 
+# Gets the height of the tallest image
+getTallestImage = ->
+  gallery.slides.addClass 'loading'
+  gallery.images.css 'max-height', 'none'
+  tallestImage = 0
+  gallery.images.each ->
+    curHeight = null
+    curHeight = $(this).height()
+    tallestImage = curHeight  if curHeight > tallestImage
+
+  gallery.slides.removeClass 'loading'
+  tallestImage
 
 # Sets max height of images so they all fit in the window
 setImageHeight = (tallestImage) ->
   windowHeight = $(window).height()
   navHeight = gallery.flexContainer.find('.flex-control-nav').outerHeight(true)
-  captionHeight = getTallestCaption()
-  bottomSpace = navHeight + captionHeight
   fixedHeight = null
   padding = 10
 
-  if windowHeight <= tallestImage + bottomSpace
+  if windowHeight <= tallestImage + navHeight
     fixedHeight = windowHeight - navHeight - padding
   else
     fixedHeight = tallestImage - padding
 
   gallery.images.css 'max-height', fixedHeight
   gallery.slides.css 'height', fixedHeight
-  gallery.flexContainer.css 'margin-bottom', bottomSpace
+  gallery.flexContainer.find('.flex-control-nav').css('bottom', -navHeight)
+  gallery.flexContainer.css 'margin-bottom', navHeight
 
 setMiniNavHeight = (tallestImage) ->
   $('.flex-direction-nav a').height(tallestImage)
@@ -76,11 +61,11 @@ setupFlexslider = (galleryOptions) ->
   tallestImage = getTallestImage()
   initializeFlexSlider(galleryOptions)
 
-  if galleryOptions['mini_gallery'] is 'no'
-    setImageHeight tallestImage
-
   if galleryOptions['mini_gallery'] is 'yes'
     setMiniNavHeight tallestImage
+
+  else
+    setImageHeight tallestImage
 
 resetFlexslider = ->
   tallestImage = getTallestImage()
@@ -90,15 +75,16 @@ resetMiniFlexslider = ->
   tallestImage = getTallestImage()
   setMiniNavHeight tallestImage
 
+
 $ ->
   galleryOptions = JSON.parse($('.gallery .config:first').html())
 
   setupFlexslider(galleryOptions)
 
-  if galleryOptions['mini_gallery'] is 'no'
-    $(window).smartresize ->
-      resetFlexslider()
-
   if galleryOptions['mini_gallery'] is 'yes'
     $(window).smartresize ->
       resetMiniFlexslider()
+
+  else
+    $(window).smartresize ->
+      resetFlexslider()
