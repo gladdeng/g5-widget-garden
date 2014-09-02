a044c6bc91375a63857d7e847a094af9a5c962ef
Chad Crissman <cdcrissman@gmail.com>
2014-09-02 16:51:55 -0400

    fixes nav js to correctly add active class

--- public/static/components/navigation/javascripts/show.js
+++ public/static/components/navigation/javascripts/show.js
@@ -1,8 +1,8 @@
 (function() {
   $(function() {
     var path;
-    path = location.pathname;
-    return $('[role=banner] .navigation a[href="' + path + '"]').addClass('active');
+    path = location.pathname.match(/([^\/]*)\/*$/)[1];
+    return $('[role=banner] .navigation a[href*="/' + path + '"]').addClass('active');
   });
 
 }).call(this);
