76f3b0aa1399269da428a0db395fc9b6d2cd14a2
johnlucia <johnmlucia@gmail.com>
2014-09-15 14:34:47 -0700

    get things dynamicized on the JS end

--- public/static/components/home-multifamily-iui/javascripts/show.js
+++ public/static/components/home-multifamily-iui/javascripts/show.js
@@ -1,34 +1,39 @@
 (function() {
-  var iuiMarkupBuilder,
-    _this = this;
+  var iuiMarkupBuilder;
 
-  $.ajax({
-    url: 'http://www.g5api.com/api/v0/multi_families?store_id=7337',
-    dataType: 'json',
-    success: function(data) {
-      var categories;
-      categories = data.unit_categories;
-      if (typeof categories !== "undefined" && categories.length > 0) {
-        return new iuiMarkupBuilder(categories);
+  $(function() {
+    var configs,
+      _this = this;
+    configs = JSON.parse($('#home-multifamily-iui-config').html());
+    return $.ajax({
+      url: "http://www.g5api.com/api/v0/multi_families?store_id=" + configs.core_store_id,
+      dataType: 'json',
+      success: function(data) {
+        var categories;
+        categories = data.unit_categories;
+        if (typeof categories !== "undefined" && categories.length > 0) {
+          return new iuiMarkupBuilder(categories, configs);
+        }
       }
-    }
+    });
   });
 
   iuiMarkupBuilder = (function() {
     var buttonTemplate;
 
-    function iuiMarkupBuilder(categories) {
+    function iuiMarkupBuilder(categories, configs) {
       var category, index, markupHash, _i, _len;
       markupHash = [];
       for (index = _i = 0, _len = categories.length; _i < _len; index = ++_i) {
         category = categories[index];
-        markupHash.push(buttonTemplate(category.beds));
+        markupHash.push(buttonTemplate(category.beds, configs));
       }
-      $('#googus').append(markupHash.join(''));
+      markupHash.push("<div class='iui-size iui-view-all'><a class='btn' href=''>View All</a></div>");
+      $('.home-multifamily-iui .iui-container').html(markupHash.join(''));
     }
 
-    buttonTemplate = function(beds) {
-      return "<h3>" + beds + "</h3>";
+    buttonTemplate = function(beds, configs) {
+      return "<div class='iui-size'><a class='btn' href='" + configs.floorplan_page_url + "#/bedrooms/" + beds + "/floorplans'>" + beds + " Bedroom</a></div>";
     };
 
     return iuiMarkupBuilder;
--- public/static/components/home-multifamily-iui/javascripts/show/show.js.coffee
+++ public/static/components/home-multifamily-iui/javascripts/show/show.js.coffee
@@ -1,22 +1,27 @@
-$.ajax
-  url: 'http://www.g5api.com/api/v0/multi_families?store_id=7337'
-  dataType: 'json'
-  success: (data) =>
-    categories = data.unit_categories
-    if typeof(categories) != "undefined" && categories.length > 0
-      new iuiMarkupBuilder(categories) 
+$ -> 
+  configs = JSON.parse($('#home-multifamily-iui-config').html())
 
-
+  $.ajax
+    url: "http://www.g5api.com/api/v0/multi_families?store_id=#{configs.core_store_id}"
+    dataType: 'json'
+    success: (data) =>
+      categories = data.unit_categories
+      if typeof(categories) != "undefined" && categories.length > 0
+        new iuiMarkupBuilder(categories, configs) 
 
 class iuiMarkupBuilder 
-  constructor: (categories) ->
+  constructor: (categories, configs) ->
     # Might need to add a sort by beds if the API isn't doing that already
     markupHash = []
 
     for category, index in categories
-      markupHash.push(buttonTemplate(category.beds))
+      markupHash.push(buttonTemplate(category.beds, configs))
 
-    $('#googus').append(markupHash.join(''))
+    markupHash.push("<div class='iui-size iui-view-all'><a class='btn' href=''>View All</a></div>")
 
-  buttonTemplate = (beds) ->
-    "<h3>#{beds}</h3>"
+    $('.home-multifamily-iui .iui-container').html(markupHash.join(''))
+
+  buttonTemplate = (beds, configs) ->
+    # Still need some Bedrooms vs Studio logic in here
+
+    "<div class='iui-size'><a class='btn' href='#{configs.floorplan_page_url}#/bedrooms/#{beds}/floorplans'>#{beds} Bedroom</a></div>"
