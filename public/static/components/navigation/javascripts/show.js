f00964b1768ebd1c2400d5c1158c6644b46c7417
Chad Crissman <cdcrissman@gmail.com>
2014-09-26 15:15:56 -0400

    adds subnav functionality

--- public/static/components/navigation/javascripts/show.js
+++ public/static/components/navigation/javascripts/show.js
@@ -8,12 +8,21 @@
         maxHeight: $(window).height() - $("header[role=banner] .collapsable-btn").outerHeight(true) + "px"
       });
     },
-    path: location.pathname.match(/([^\/]*)\/*$/)[1]
+    path: location.pathname.match(/([^\/]*)\/*$/)[1],
+    setupSubNav: function() {
+      return $('.has-subnav > a').on('click', function(e) {
+        $(this).next().toggleClass('show-subnav');
+        return false;
+      });
+    }
   };
 
   $(function() {
     NAVIGATION.menu.find("a[href$=\"/" + NAVIGATION.path + "\"]").addClass("active");
     NAVIGATION.setMenuHeight();
+    if ($('.has-subnav').length > 0) {
+      NAVIGATION.setupSubNav();
+    }
     return $(window).smartresize(function() {
       return NAVIGATION.setMenuHeight();
     });
--- public/static/components/navigation/javascripts/show/show.js.coffee
+++ public/static/components/navigation/javascripts/show/show.js.coffee
@@ -1,13 +1,23 @@
 NAVIGATION =
   menu: $("#drop-target-nav .navigation")
+
   setMenuHeight: ->
     @menu.css maxHeight: $(window).height() - $("header[role=banner] .collapsable-btn").outerHeight(true) + "px"
+
   path: location.pathname.match(/([^\/]*)\/*$/)[1]
+
+  setupSubNav: ->
+    $('.has-subnav > a').on 'click', (e) ->
+      $(this).next().toggleClass 'show-subnav'
+      return false
 
 $ ->
 
   NAVIGATION.menu.find("a[href$=\"/" + NAVIGATION.path + "\"]").addClass "active"
   NAVIGATION.setMenuHeight()
+
+  if $('.has-subnav').length > 0
+    NAVIGATION.setupSubNav()
 
   $(window).smartresize ->
     NAVIGATION.setMenuHeight()
