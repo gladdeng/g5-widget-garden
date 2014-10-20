49a71b30990a7f6d9f23f2bd6a362c687e0b872b
johnlucia <johnmlucia@gmail.com>
2014-10-20 09:24:56 -0700

    a little more on the dynamic edit view

--- public/static/components/featured-properties/javascripts/edit.js
+++ public/static/components/featured-properties/javascripts/edit.js
@@ -1,6 +1,30 @@
 (function() {
+  var inputFields;
+
+  inputFields = (function() {
+    function inputFields(photo) {
+      this.photo = photo;
+    }
+
+    inputFields.prototype.markup = function() {
+      return "<div>*** " + this.photo.url + " ***</div>";
+    };
+
+    return inputFields;
+
+  })();
+
   $(function() {
-    return alert("Here comes the edit.js");
+    var galleryOptions, index, markup, photo, photoMarkup, _i, _len, _ref;
+    galleryOptions = JSON.parse($('.featured-properties-config').html());
+    markup = "";
+    _ref = galleryOptions.photos;
+    for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+      photo = _ref[index];
+      photoMarkup = new inputFields(photo);
+      markup += photoMarkup.markup();
+    }
+    return $('.photo-fields').append(markup);
   });
 
 }).call(this);
--- public/static/components/featured-properties/javascripts/edit/edit.js.coffee
+++ public/static/components/featured-properties/javascripts/edit/edit.js.coffee
@@ -1,2 +1,26 @@
+class inputFields
+  constructor: (@photo) ->
+
+  markup: ->
+    "<div>*** #{@photo.url} ***</div>"
+
 $ -> 
-  alert "Here comes the edit.js"
+  galleryOptions = JSON.parse($('.featured-properties-config').html())
+  markup = ""
+
+  for photo, index in galleryOptions.photos
+    photoMarkup = new inputFields photo
+    markup += photoMarkup.markup()
+
+  $('.photo-fields').append(markup)
+
+ 
+
+
+
+
+
+
+
+
+
