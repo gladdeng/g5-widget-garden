6c169a0fc6c43c4576aa029cbb7c38c59f1cd556
Chad Crissman <cdcrissman@gmail.com>
2014-10-31 13:43:33 -0400

    loads next image instead of current image

--- public/static/components/gallery-basic/javascripts/show.js
+++ public/static/components/gallery-basic/javascripts/show.js
@@ -2,27 +2,21 @@
   var nextImage;
 
   nextImage = function() {
-    var $active, $img, $next, loading, src;
+    var $active, $img, $next, $nextLoad, src;
     $active = $(".gallery-basic .active");
     $next = ($active.next().length > 0 ? $active.next() : $(".gallery-basic figure:first"));
-    loading = false;
-    if ($next.hasClass("lazy-load")) {
-      loading = true;
-      $img = $next.find("img");
+    $nextLoad = ($next.next().length > 0 ? $next.next() : $(".gallery-basic figure:first"));
+    if ($nextLoad.hasClass("lazy-load")) {
+      $img = $nextLoad.find("img");
       src = $img.attr("data-src");
       $img.attr("src", src);
-      $next.removeClass("lazy-load");
-      $img.on('load', function() {
-        return loading = false;
-      });
+      $nextLoad.removeClass("lazy-load");
     }
-    if (loading === false) {
-      $next.css("z-index", 2);
-      return $active.fadeOut(1500, function() {
-        $active.css("z-index", 1).show().removeClass("active");
-        return $next.css("z-index", 3).addClass("active");
-      });
-    }
+    $next.css("z-index", 2);
+    return $active.fadeOut(1500, function() {
+      $active.css("z-index", 1).show().removeClass("active");
+      return $next.css("z-index", 3).addClass("active");
+    });
   };
 
   setInterval((function() {
--- public/static/components/gallery-basic/javascripts/show/show.js.coffee
+++ public/static/components/gallery-basic/javascripts/show/show.js.coffee
@@ -1,24 +1,20 @@
 nextImage = ->
   $active = $(".gallery-basic .active")
   $next = (if $active.next().length > 0 then $active.next() else $(".gallery-basic figure:first"))
-  loading = false # keeps track of whether an image is being loaded
+  $nextLoad = (if $next.next().length > 0 then $next.next() else $(".gallery-basic figure:first"))
 
   # check if next image needs to be loaded
-  if $next.hasClass "lazy-load"
-    loading = true
-    $img = $next.find "img"
+  if $nextLoad.hasClass "lazy-load"
+    $img = $nextLoad.find "img"
     src = $img.attr "data-src"
     $img.attr "src", src
-    $next.removeClass "lazy-load"
-    $img.on 'load', ->
-      loading = false
+    $nextLoad.removeClass "lazy-load"
 
   # when images are done loading, transition to next slide
-  if loading is false
-    $next.css "z-index", 2
-    $active.fadeOut 1500, ->
-      $active.css("z-index", 1).show().removeClass "active"
-      $next.css("z-index", 3).addClass "active"
+  $next.css "z-index", 2
+  $active.fadeOut 1500, ->
+    $active.css("z-index", 1).show().removeClass "active"
+    $next.css("z-index", 3).addClass "active"
 
 
 # Change slide every 7 seconds
