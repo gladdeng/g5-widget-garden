f46be6428f16a0e5041d610f044472a7755ef4fe
johnlucia <johnmlucia@gmail.com>
2014-11-03 07:20:37 -0800

    luke warm microformat action

---
+++ public/static/components/city-state-zip-search/javascripts/show.js
@@ -0,0 +1,6 @@
+(function() {
+  $(function() {
+    return alert("vegan, straightedge, shit for brains");
+  });
+
+}).call(this);
---
+++ public/static/components/city-state-zip-search/javascripts/show/show.js.coffee
@@ -0,0 +1,3 @@
+$ ->
+
+  alert "vegan, straightedge, shit for brains"
