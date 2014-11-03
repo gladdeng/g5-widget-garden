96201c44b19ea52bbc03881a818daa9ad8db6dae
johnlucia <johnmlucia@gmail.com>
2014-11-03 11:14:49 -0800

    the guts of the js working

--- public/static/components/city-state-zip-search/javascripts/show.js
+++ public/static/components/city-state-zip-search/javascripts/show.js
@@ -1,6 +1,91 @@
 (function() {
+  var ZipSearchConfigs, searchResultsList;
+
   $(function() {
-    return alert("vegan, straightedge, shit for brains");
+    var zipSearchConfigs,
+      _this = this;
+    zipSearchConfigs = new ZipSearchConfigs;
+    if (zipSearchConfigs.searchURL()) {
+      return $.ajax({
+        url: zipSearchConfigs.searchURL(),
+        dataType: 'json',
+        success: function(data) {
+          return new searchResultsList(zipSearchConfigs, data);
+        }
+      });
+    }
   });
+
+  searchResultsList = (function() {
+    function searchResultsList(zipSearchConfigs, data) {
+      this.zipSearchConfigs = zipSearchConfigs;
+      this.data = data;
+      this.populateResults();
+    }
+
+    searchResultsList.prototype.populateResults = function() {
+      var index, location, markupHash, _i, _len, _ref;
+      markupHash = [];
+      if (this.data.success) {
+        markupHash.push("<p>We have " + this.data.locations.length + " locations near " + (this.zipSearchConfigs.searchArea()) + ":</p>");
+      } else {
+        markupHash.push("<p>Sorry, we don't have any locations in that area. Please try a different search, or see our full list of locations below:</p>");
+      }
+      _ref = this.data.locations;
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        location = _ref[index];
+        markupHash.push("<div>");
+        markupHash.push(" <p>" + location.name + "</p>                        <p>" + location.street_address_1 + "</p>                        <p>" + location.city + "</p>                        <p>" + location.state + "</p>                        <p>" + location.domain + "</p> ");
+        markupHash.push("</div>");
+      }
+      return $('.city-state-zip-search').append(markupHash.join(''));
+    };
+
+    return searchResultsList;
+
+  })();
+
+  ZipSearchConfigs = (function() {
+    function ZipSearchConfigs() {
+      this.configs = JSON.parse($('#zip-search-config').html());
+    }
+
+    ZipSearchConfigs.prototype.getParameter = function(name) {
+      var regex, results, value;
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+      results = regex.exec(location.search);
+      return value = results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    };
+
+    ZipSearchConfigs.prototype.searchURL = function() {
+      var radius, search, searchURL;
+      search = this.getParameter("search");
+      radius = this.getParameter("radius");
+      if (search === "") {
+        searchURL = null;
+      } else {
+        searchURL = "" + this.configs.serviceURL + "/clients/" + this.configs.clientURN + "/location_search.json?";
+        searchURL += "search=" + search;
+        if (radius !== "") {
+          searchURL += "&radius=" + radius;
+        }
+      }
+      return searchURL;
+    };
+
+    ZipSearchConfigs.prototype.searchArea = function() {
+      var search;
+      search = this.getParameter('search');
+      if (search) {
+        return search.toUpperCase();
+      } else {
+        return "";
+      }
+    };
+
+    return ZipSearchConfigs;
+
+  })();
 
 }).call(this);
--- public/static/components/city-state-zip-search/javascripts/show/show.js.coffee
+++ public/static/components/city-state-zip-search/javascripts/show/show.js.coffee
@@ -1,3 +1,76 @@
 $ ->
+  zipSearchConfigs = new ZipSearchConfigs
+  new searchButtonListener(zipSearchConfigs)
 
-  alert "vegan, straightedge, shit for brains"
+  if zipSearchConfigs.searchURL()
+    $.ajax
+      url: zipSearchConfigs.searchURL()
+      dataType: 'json'
+      success: (data) =>
+        new searchResultsList(zipSearchConfigs, data)
+  
+class searchResultsList
+  constructor: (@zipSearchConfigs, @data) ->
+    @populateResults()
+
+  populateResults: () ->
+    markupHash = []
+
+    if @data.success
+      markupHash.push("<p>We have #{@data.locations.length} locations near #{ @zipSearchConfigs.searchArea() }:</p>")
+    else
+      markupHash.push("<p>Sorry, we don't have any locations in that area. Please try a different search, or see our full list of locations below:</p>")
+
+    for location, index in @data.locations
+      markupHash.push("<div>")
+      markupHash.push(" <p>#{location.name}</p>
+                        <p>#{location.street_address_1}</p>
+                        <p>#{location.city}</p>
+                        <p>#{location.state}</p>
+                        <p>#{location.domain}</p> ")
+      markupHash.push("</div>")
+
+    $('.city-state-zip-search').append(markupHash.join(''))
+
+class ZipSearchConfigs
+  constructor: () ->
+    @configs = JSON.parse($('#zip-search-config').html())
+
+  getParameter: (name) ->
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]")
+    regex = new RegExp("[\\?&]" + name + "=([^&#]*)")
+    results = regex.exec(location.search)
+    value = if results == null then "" else decodeURIComponent(results[1].replace(/\+/g, " "))
+
+  searchURL: () ->
+    search = @getParameter("search")
+    radius = @getParameter("radius")
+
+    if search == ""
+      searchURL = null
+    else
+      searchURL = "#{@configs.serviceURL}/clients/#{@configs.clientURN}/location_search.json?"
+      searchURL += "search=#{search}"
+      searchURL += "&radius=#{radius}" if radius != ""
+    
+    searchURL
+
+  searchArea: () ->
+    search = @getParameter('search')
+
+    if search
+      search.toUpperCase()
+    else
+      ""
+class searchButtonListener
+  constructor: (@zipSearchConfigs) ->
+    # set up the listener for the search button
+    # account for mini and full versions
+
+
+
+  
+
+
+
+
