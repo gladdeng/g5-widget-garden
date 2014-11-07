2046a69d80275a65cea4244249382c637a7452ca
bbauer <brian.bauer@g5platform.com>
2014-11-07 14:44:44 -0800

    Checkin compiled navigation js

--- public/static/components/navigation/javascripts/show.js
+++ public/static/components/navigation/javascripts/show.js
@@ -1,38 +1,69 @@
 (function() {
-  var NAVIGATION;
-
-  NAVIGATION = {
-    menu: $("#drop-target-nav .navigation"),
-    setMenuHeight: function() {
-      return this.menu.css({
-        maxHeight: $(window).height() - $("header[role=banner] .collapsable-btn").outerHeight(true) + "px"
+  (function() {
+    var NAVIGATION;
+    NAVIGATION = void 0;
+    NAVIGATION = {
+      corporateMenu: $("#drop-target-nav .corporate-navigation"),
+      locationMenu: $("#drop-target-nav .navigation"),
+      path: location.pathname.match(/([^\/]*)\/*$/)[1],
+      setActiveMenu: function(menu) {
+        menu.find("a[href$=\"/" + this.path + "\"]").addClass("active");
+      },
+      setMenuHeight: function(menu) {
+        menu.css({
+          maxHeight: $(window).height() - $("header[role=banner] .collapsable-btn").outerHeight(true) + "px"
+        });
+      },
+      setupMenu: function(menu) {
+        this.setActiveMenu(menu);
+        this.setMenuHeight(menu);
+        if (menu.find(".has-subnav").length > 0) {
+          this.setupSubNav(menu);
+        }
+      },
+      setupSubNav: function(menu) {
+        menu.find(".has-subnav > a").on("click", function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+          NAVIGATION.closeSubNav(NAVIGATION.corporateMenu.find(".subnav").not($(this).next()));
+          NAVIGATION.closeSubNav(NAVIGATION.locationMenu.find(".subnav").not($(this).next()));
+          NAVIGATION.toggleSubNav($(this).next());
+        });
+      },
+      closeSubNav: function(subnav) {
+        subnav.removeClass("show-subnav");
+        subnav.parent().removeClass("subnav-open");
+      },
+      toggleSubNav: function(subnav) {
+        subnav.toggleClass("show-subnav");
+        subnav.parent().toggleClass("subnav-open");
+      },
+      resetSubNav: function() {
+        NAVIGATION.closeSubNav(this.corporateMenu.find(".subnav"));
+        NAVIGATION.closeSubNav(this.locationMenu.find(".subnav"));
+      }
+    };
+    $(function() {
+      if (NAVIGATION.corporateMenu.length > 0) {
+        NAVIGATION.setupMenu(NAVIGATION.corporateMenu);
+      }
+      if (NAVIGATION.locationMenu.length > 0) {
+        NAVIGATION.setupMenu(NAVIGATION.locationMenu);
+      }
+      if ($(".has-subnav").length > 0) {
+        $("body").on("click", function(e) {
+          NAVIGATION.resetSubNav();
+        });
+      }
+      $(window).smartresize(function() {
+        if (NAVIGATION.corporateMenu.length > 0) {
+          NAVIGATION.setMenuHeight(NAVIGATION.corporateMenu);
+        }
+        if (NAVIGATION.locationMenu.length > 0) {
+          NAVIGATION.setMenuHeight(NAVIGATION.locationMenu);
+        }
       });
-    },
-    path: location.pathname.match(/([^\/]*)\/*$/)[1],
-    setupSubNav: function() {
-      return $('.has-subnav > a').on('click', function(e) {
-        NAVIGATION.menu.find('.subnav').not($(this).next()).removeClass('show-subnav');
-        $(this).next().toggleClass('show-subnav');
-        return false;
-      });
-    },
-    closeSubNav: function() {
-      return this.menu.find('.show-subnav').removeClass('show-subnav');
-    }
-  };
-
-  $(function() {
-    NAVIGATION.menu.find("a[href$=\"/" + NAVIGATION.path + "\"]").addClass("active");
-    NAVIGATION.setMenuHeight();
-    if ($('.has-subnav').length > 0) {
-      NAVIGATION.setupSubNav();
-      $('body').on('click', function(e) {
-        return NAVIGATION.closeSubNav();
-      });
-    }
-    return $(window).smartresize(function() {
-      return NAVIGATION.setMenuHeight();
     });
-  });
+  }).call(this);
 
 }).call(this);
