c6240947c223ca3e57d9861422b251a23b3c6ee2
johnlucia <johnmlucia@gmail.com>
2014-12-02 17:41:15 -0800

    switch over to coffescript

--- public/static/components/google-translate/javascripts/show.js
+++ public/static/components/google-translate/javascripts/show.js
@@ -1,3 +1,17 @@
-$( document ).ready(function() {
-   alert("hey, look, it's working");
-});
+(function() {
+  var googleTranslateElementInit;
+
+  googleTranslateElementInit = function() {
+    return new google.translate.TranslateElement({
+      pageLanguage: 'en',
+      includedLanguages: 'cs,de,en,es,fr,hr,hy,id,it,iw,ja,ko,lt,pl,ru,tl,zh-CN',
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+      autoDisplay: false
+    }, 'google_translate_element');
+  };
+
+  $(function() {
+    return googleTranslateElementInit();
+  });
+
+}).call(this);
---
+++ public/static/components/google-translate/javascripts/show/show.js.coffee
@@ -0,0 +1,5 @@
+googleTranslateElementInit = () ->
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'cs,de,en,es,fr,hr,hy,id,it,iw,ja,ko,lt,pl,ru,tl,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element')
+
+$ ->
+  googleTranslateElementInit()
