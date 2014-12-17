531526995baed67f79cadb38744b1b8c9d036732
Dave Reuss <davereuss@Dave-Reusss-MacBook-Pro.local>
2014-12-17 11:23:00 -0800

    Add - refactor js

--- public/static/components/walkscore/javascripts/show.js
+++ public/static/components/walkscore/javascripts/show.js
@@ -1,40 +1,41 @@
 (function() {
-  var walkscoreBadgeBuilder, walkscoreInitializer;
+  var walkscoreInitializer;
 
   $(function() {
     var walkFeedVars, walkscoreFeed;
-    walkFeedVars = JSON.parse($("#walkscore-config:first").html());
+    walkFeedVars = JSON.parse($("#walkscore-config").html());
     if (walkFeedVars.walkscore_client.length > 1) {
       return walkscoreFeed = new walkscoreInitializer(walkFeedVars);
     }
   });
 
   walkscoreInitializer = (function() {
-    var getpage;
-
     function walkscoreInitializer(walkFeedVars) {
-      return getpage(walkFeedVars);
+      this.getpage(walkFeedVars);
     }
 
-    getpage = function(walkFeedVars) {
+    walkscoreInitializer.prototype.getpage = function(walkFeedVars) {
       var _this = this;
       return $.ajax({
-        url: "http://localhost:3008/walkscore-feed/" + walkFeedVars.walkscore_client + "/" + walkFeedVars.walkscore_location,
+        url: "http://localhost:3008/walkscore-feed/" + walkFeedVars.walkscore_client + "/" + walkFeedVars.walkscore_location + "sfdsfs",
         dataType: 'json',
         success: function(data) {
-          return walkscoreBadgeBuilder(data);
+          debugger;
+          return _this.walkscoreBadgeBuilder(data);
         }
       });
+    };
+
+    walkscoreInitializer.prototype.walkscoreBadgeBuilder = function(dataFeed) {
+      var walkscoreBlock;
+      if (dataFeed !== []) {
+        walkscoreBlock = "<a href=\"" + dataFeed.ws_link + "\" target=\"_blank\">\n <img src=\"" + dataFeed.logo_url + "\" alt=\"Walkscore Logo\"/>\n   <span>" + dataFeed.walkscore + "</span>\n</a>";
+        return $('.walkscore-listing').append(walkscoreBlock);
+      }
     };
 
     return walkscoreInitializer;
 
   })();
-
-  walkscoreBadgeBuilder = function(dataFeed) {
-    var walkscoreBlock;
-    walkscoreBlock = "<a href=\"" + dataFeed.ws_link + "\" target=\"_blank\">\n <img src=\"" + dataFeed.logo_url + "\" alt=\"Walkscore Logo\"/>\n   <span>" + dataFeed.walkscore + "</span>\n</a>";
-    return $('.walkscore-listing').append(walkscoreBlock);
-  };
 
 }).call(this);
--- public/static/components/walkscore/javascripts/show/show.js.coffee
+++ public/static/components/walkscore/javascripts/show/show.js.coffee
@@ -1,6 +1,6 @@
 $ ->
   # Get walkscore config options
-  walkFeedVars = JSON.parse($("#walkscore-config:first").html())
+  walkFeedVars = JSON.parse($("#walkscore-config").html())
 
   # Walkscore Setup
   if walkFeedVars.walkscore_client.length > 1
@@ -15,18 +15,20 @@ class walkscoreInitializer
   getpage = (walkFeedVars) ->
     $.ajax
       #url: "//g5-social-feed-service.herokuapp.com/walkscore-feed/#{walkFeedVars.walkscore_client}/#{walkFeedVars.walkscore_location}"
-      url: "http://localhost:3008/walkscore-feed/#{walkFeedVars.walkscore_client}/#{walkFeedVars.walkscore_location}"
+      url: "http://localhost:3008/walkscore-feed/#{walkFeedVars.walkscore_client}/#{walkFeedVars.walkscore_location}sdfds"
       dataType: 'json'
       success: (data) =>
+        debugger
         #console.log(JSON.stringify(data))
         #walkscoreBadgeBuilder(walkFeedVars, data) if data.length > 0
         walkscoreBadgeBuilder(data)
 
 
-walkscoreBadgeBuilder = (dataFeed) ->
-  walkscoreBlock = """<a href="#{dataFeed.ws_link}" target="_blank">
-                       <img src="#{dataFeed.logo_url}" alt="Walkscore Logo"/>
-                         <span>#{dataFeed.walkscore}</span>
-                      </a>
-                  """
-  $('.walkscore-listing').append(walkscoreBlock)
+  walkscoreBadgeBuilder = (dataFeed) ->
+    if dataFeed != []
+      walkscoreBlock = """<a href="#{dataFeed.ws_link}" target="_blank">
+                           <img src="#{dataFeed.logo_url}" alt="Walkscore Logo"/>
+                             <span>#{dataFeed.walkscore}</span>
+                          </a>
+                      """
+      $('.walkscore-listing').append(walkscoreBlock)
