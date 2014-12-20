1bfc7e21458e0a2a8aa4ece8703740c49ee2d2de
johnlucia <johnmlucia@gmail.com>
2014-12-19 17:46:35 -0800

    basic widget function

--- public/static/components/news-feed/javascripts/show.js
+++ public/static/components/news-feed/javascripts/show.js
@@ -1,27 +1,39 @@
 (function() {
-  var FeedSlurper;
+  var NewsFeedBuilder;
 
   $(function() {
-    var configs, newsFeed;
+    var configs,
+      _this = this;
     configs = JSON.parse($('#news-feed-config').html());
-    return newsFeed = new FeedSlurper(configs);
+    return $.ajax({
+      url: "" + configs.newsServiceDomain + "/locations/" + configs.locationURN + "/news_feed.json",
+      dataType: 'json',
+      success: function(data) {
+        return new NewsFeedBuilder(configs, data);
+      }
+    });
   });
 
-  FeedSlurper = (function() {
-    function FeedSlurper(configs) {
-      var _this = this;
+  NewsFeedBuilder = (function() {
+    function NewsFeedBuilder(configs, feed) {
       this.configs = configs;
-      $.ajax({
-        url: "" + this.configs.newsServiceDomain + "/locations/" + this.configs.locationURN + "/news_feed.json",
-        dataType: 'json',
-        success: function(data) {
-          alert("(.Y.)");
-          debugger;
-        }
-      });
+      this.feed = feed;
+      this.populateFeed();
     }
 
-    return FeedSlurper;
+    NewsFeedBuilder.prototype.populateFeed = function() {
+      var index, markup, post, _i, _len, _ref;
+      markup = [];
+      markup.push("<h2>Fuckin-A</h2>");
+      _ref = this.feed;
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        post = _ref[index];
+        markup.push("<div class='news-feed-post'>                      <img src='" + post.image + "' />                      <div>" + post.title + "</div>                      <div>" + post.author + "</div>                      <div>" + post.date + "</div>                      <div>" + post.description + "</div>                      <div>" + post.text + "</div>                    </div>");
+      }
+      return $('.news-feed-widget').append(markup.join(''));
+    };
+
+    return NewsFeedBuilder;
 
   })();
 
--- public/static/components/news-feed/javascripts/show/show.js.coffee
+++ public/static/components/news-feed/javascripts/show/show.js.coffee
@@ -1,14 +1,40 @@
 $ ->
   configs = JSON.parse($('#news-feed-config').html())
-  newsFeed = new FeedSlurper(configs)
+  $.ajax
+    url: "#{configs.newsServiceDomain}/locations/#{configs.locationURN}/news_feed.json"
+    dataType: 'json'
+    success: (data) =>
+      new NewsFeedBuilder(configs, data)
+
+class NewsFeedBuilder
+  constructor: (@configs, @feed) ->
+    @populateFeed()
+
+  populateFeed: () ->
+    markup = []
+    markup.push("<h2>Fuckin-A</h2>")
+
+    for post, index in @feed
+      markup.push( "<div class='news-feed-post'>
+                      <img src='#{post.image}' />
+                      <div>#{post.title}</div>
+                      <div>#{post.author}</div>
+                      <div>#{post.date}</div>
+                      <div>#{post.description}</div>
+                      <div>#{post.text}</div>
+                    </div>" )
+      
+    $('.news-feed-widget').append(markup.join(''))
 
 
-class FeedSlurper
-  constructor: (@configs) ->
-    $.ajax
-      url: "#{@configs.newsServiceDomain}/locations/#{@configs.locationURN}/news_feed.json"
-      dataType: 'json'
-      success: (data) =>
-        alert "made it"
-        debugger
+
+      
+
+
+  
+
+
+        
+
+    
 
