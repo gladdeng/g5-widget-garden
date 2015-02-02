28d0907119e85bea41b12f69cdf5f4b635f728e7
Dave Reuss <davereuss@Dave-Reusss-MacBook-Pro.local>
2015-02-02 14:48:43 -0800

    Add - Realpage Widget

---
+++ public/static/components/real-page/javascripts/show.js
@@ -0,0 +1,10 @@
+(function() {
+  $(function() {
+    var realPageVars;
+    realPageVars = realPageVarsConfig;
+    if (realPageVars.realpage_id.length > 1) {
+      return console.log(realPageVars.realpage_id);
+    }
+  });
+
+}).call(this);
---
+++ public/static/components/real-page/javascripts/show/show.js.coffee
@@ -0,0 +1,6 @@
+$ ->
+  # Get realpage config options
+  realPageVars = realPageVarsConfig   
+  # RealPage Setup
+  if realPageVars.realpage_id.length > 1
+    console.log(realPageVars.realpage_id)
