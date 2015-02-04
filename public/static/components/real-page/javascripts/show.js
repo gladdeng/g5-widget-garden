05c0657778a8210b47ff5c367c348d11f4486e9f
Dave Reuss <davereuss@Dave-Reusss-MacBook-Pro.local>
2015-02-04 09:13:43 -0800

    Mod - better var name

--- public/static/components/real-page/javascripts/show.js
+++ public/static/components/real-page/javascripts/show.js
@@ -3,10 +3,10 @@
     var realPageJSBuilder, realPageVars;
     realPageVars = realPageVarsConfig;
     realPageJSBuilder = function(configs) {
-      var realPageJS, realpage_url, target_div;
+      var realPageJS, realpage_url, target_div_param;
       console.log(configs);
-      target_div = "&container=realpage-iframe";
-      realpage_url = configs.realpage_stylesheet.length > 1 && configs.realpage_id.length > 1 ? "" + configs.realpage_id + target_div + "&css=https%3A" + configs.realpage_stylesheet : configs.realpage_id.length > 1 ? "" + configs.realpage_id + target_div : void 0;
+      target_div_param = "&container=realpage-iframe";
+      realpage_url = configs.realpage_stylesheet.length > 1 && configs.realpage_id.length > 1 ? "" + configs.realpage_id + target_div_param + "&css=https%3A" + configs.realpage_stylesheet : configs.realpage_id.length > 1 ? "" + configs.realpage_id + target_div_param : void 0;
       console.log(realpage_url);
       realPageJS = "<SCRIPT language=\"javascript\" src=\"https://property.onesite.realpage.com/oll/eol/widget?siteId=" + realpage_url + "\"></SCRIPT>";
       if (realpage_url != null) {
