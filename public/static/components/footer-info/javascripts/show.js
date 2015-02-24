04d2e7bc03ea3226f814d6bc7ecee83e68df7387
Chad Crissman <cdcrissman@gmail.com>
2015-02-24 16:22:03 -0500

    revert change to footer info js

--- public/static/components/footer-info/javascripts/show.js
+++ public/static/components/footer-info/javascripts/show.js
@@ -5,6 +5,9 @@
 
   if ($(".social-links.widget").length < 1) {
     localPath = window.location.href;
+    if (window.location.hash) {
+      localPath = localPath.replace(window.location.hash, '');
+    }
     $.each($(".social-links use"), function() {
       $(this).attr("xlink:href", localPath + $(this).attr("xlink:href"));
     });
